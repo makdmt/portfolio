e4971824adfedfc16c63906e5621d12cafa2c65d
Dmitry Makarov <makdmt@gmail.com>
2025-09-19 17:34:52 +0300

    feat(components) add mobilePreview component

---
+++ app/components/mobilePreview.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import {useImagePreload} from "../composables/useImagePreload";
+
+const props = defineProps<{
+  srcHigh: string;
+  srcLow?: string;
+  alt: string;
+}>();
+
+const {loaded} = useImagePreload(props.srcHigh);
+
+</script>
+
+<template>
+  <img
+      :src="loaded ? srcHigh : srcLow || srcHigh"
+      :alt="alt"
+      class="image"
+  />
+</template>
+
+<style scoped>
+
+.image {
+  border: 4px solid var(--white);
+  border-radius: 32px;
+  width: 222px;
+  height: 493px;
+  box-shadow: 17px 17px 48px #0000001f;
+}
+
+</style>
