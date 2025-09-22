83b9dbc1ff81eee0bc89cdbea267bd5b3cfd1fce
Dmitry Makarov <makdmt@gmail.com>
2025-09-22 17:40:39 +0300

    feat(components) add qualityAssured component

---
+++ app/components/qualityAssured.vue
@@ -0,0 +1,269 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="quality_container">
+    <div class="leaves_container">
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="22" height="40"
+           class="g-icon" fill="currentColor" stroke="none" aria-hidden="true">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 40">
+          <path fill="url(#rating-badge-bronze-left_svg__a)"
+                d="M20.927 31.082c0 2.537-.81 3.783-2.162 4.594-.809-.54-2.16-2.057-2.16-4.594s1.35-4.054 2.16-4.594c.541.27 2.162 2.057 2.162 4.594"></path>
+          <path fill="url(#rating-badge-bronze-left_svg__b)"
+                d="M17.274 26.442c-.899 2.373-2.098 3.252-3.65 3.531-.564-.792-1.29-2.688-.392-5.061.899-2.373 2.698-3.313 3.647-3.532.41.445 1.293 2.69.395 5.062"></path>
+          <path fill="url(#rating-badge-bronze-left_svg__c)"
+                d="M14.37 20.584c-1.086 2.293-2.351 3.073-3.92 3.227-.5-.834-1.072-2.782.013-5.076s2.954-3.087 3.917-3.229c.374.476 1.075 2.784-.01 5.078"></path>
+          <path fill="url(#rating-badge-bronze-left_svg__d)"
+                d="M14.367 13.246c-1.4 2.117-2.763 2.709-4.338 2.64-.376-.898-.666-2.908.734-5.024s3.362-2.636 4.336-2.64c.302.524.668 2.908-.732 5.024"></path>
+          <path fill="url(#rating-badge-bronze-left_svg__e)"
+                d="M6.215 9.226c.804 2.407.43 3.845-.595 5.042-.938-.256-2.7-1.266-3.504-3.673s-.004-4.272.593-5.042c.6.085 2.703 1.266 3.506 3.673"></path>
+          <path fill="url(#rating-badge-bronze-left_svg__f)"
+                d="M4.67 15.665c1.45 2.082 1.498 3.567.852 5.005-.973.019-2.948-.454-4.398-2.536S-.085 14.036.271 13.13c.599-.088 2.95.452 4.4 2.534"></path>
+          <path fill="url(#rating-badge-bronze-left_svg__g)"
+                d="M5.887 22.561c1.872 1.712 2.246 3.15 1.932 4.695-.944.232-2.975.207-4.848-1.505s-2.082-3.73-1.934-4.694c.564-.217 2.977-.208 4.85 1.504"></path>
+          <path fill="url(#rating-badge-bronze-left_svg__h)"
+                d="M7.752 28.472c2.288 1.096 3.062 2.364 3.21 3.934-.837.496-2.788 1.06-5.076-.036-2.289-1.095-3.074-2.968-3.212-3.932.478-.371 2.79-1.062 5.078.034"></path>
+          <path fill="url(#rating-badge-bronze-left_svg__i)"
+                d="M12.204 34.226c2.442.692 3.42 1.81 3.831 3.332-.74.631-2.568 1.518-5.009.826-2.44-.691-3.532-2.403-3.831-3.33.407-.447 2.568-1.52 5.01-.828"></path>
+          <path fill="url(#rating-badge-bronze-left_svg__j)"
+                d="M12.474 4.987C11.844 7.445 10.75 8.451 9.24 8.9c-.65-.724-1.581-2.529-.951-4.986C8.918 1.456 10.602.322 11.52 0c.457.396 1.583 2.53.953 4.987"></path>
+          <path fill="url(#rating-badge-bronze-left_svg__k)"
+                d="m8.227 10.003 1.622.81c-3.073 11.483.316 17.513 11.35 27.836L20.118 40C9.398 29.33 4.808 23.097 8.228 10.003"></path>
+          <defs>
+            <linearGradient id="rating-badge-bronze-left_svg__a" x1="0" x2="51" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-left_svg__b" x1="0" x2="51" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-left_svg__c" x1="0" x2="51" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-left_svg__d" x1="0" x2="51" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-left_svg__e" x1="0" x2="51" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-left_svg__f" x1="0" x2="51" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-left_svg__g" x1="0" x2="51" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-left_svg__h" x1="0" x2="51" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-left_svg__i" x1="0" x2="51" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-left_svg__j" x1="0" x2="51" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-left_svg__k" x1="0" x2="51" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+          </defs>
+        </svg>
+      </svg>
+      <div class="RatingValue-module__text--dMfYt"><p>100%</p></div>
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="22" height="40"
+           class="g-icon" fill="currentColor" stroke="none" aria-hidden="true">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 22 40">
+          <path fill="url(#rating-badge-bronze-right_svg__a)"
+                d="M.272 31.082c0 2.537.81 3.783 2.162 4.594.808-.54 2.16-2.057 2.16-4.594s-1.35-4.054-2.16-4.594c-.541.27-2.162 2.057-2.162 4.594"></path>
+          <path fill="url(#rating-badge-bronze-right_svg__b)"
+                d="M3.925 26.442c.899 2.373 2.098 3.252 3.649 3.531.565-.792 1.291-2.688.393-5.061-.899-2.373-2.698-3.313-3.647-3.532-.41.445-1.294 2.69-.395 5.062"></path>
+          <path fill="url(#rating-badge-bronze-right_svg__c)"
+                d="M6.83 20.584c1.085 2.293 2.35 3.073 3.919 3.227.5-.834 1.072-2.782-.013-5.076s-2.954-3.087-3.918-3.229c-.373.476-1.074 2.784.011 5.078"></path>
+          <path fill="url(#rating-badge-bronze-right_svg__d)"
+                d="M6.832 13.246c1.4 2.117 2.763 2.709 4.337 2.64.377-.898.667-2.908-.733-5.024S7.074 8.226 6.1 8.222c-.302.524-.668 2.908.732 5.024"></path>
+          <path fill="url(#rating-badge-bronze-right_svg__e)"
+                d="M14.983 9.226c-.803 2.407-.43 3.845.596 5.042.938-.256 2.7-1.266 3.504-3.673s.004-4.272-.594-5.042c-.598.085-2.702 1.266-3.506 3.673"></path>
+          <path fill="url(#rating-badge-bronze-right_svg__f)"
+                d="M16.528 15.665c-1.45 2.082-1.497 3.567-.851 5.005.972.019 2.948-.454 4.398-2.536s1.208-4.098.852-5.004c-.598-.088-2.95.452-4.399 2.534"></path>
+          <path fill="url(#rating-badge-bronze-right_svg__g)"
+                d="M15.312 22.561c-1.873 1.712-2.246 3.15-1.932 4.695.944.232 2.975.207 4.848-1.505s2.081-3.73 1.934-4.694c-.565-.217-2.977-.208-4.85 1.504"></path>
+          <path fill="url(#rating-badge-bronze-right_svg__h)"
+                d="M13.447 28.472c-2.289 1.096-3.063 2.364-3.21 3.934.836.496 2.788 1.06 5.076-.036 2.289-1.095 3.074-2.968 3.211-3.932-.477-.371-2.789-1.062-5.077.034"></path>
+          <path fill="url(#rating-badge-bronze-right_svg__i)"
+                d="M8.994 34.226c-2.44.692-3.419 1.81-3.83 3.332.74.631 2.567 1.518 5.008.826 2.442-.691 3.533-2.403 3.832-3.33-.408-.447-2.568-1.52-5.01-.828"></path>
+          <path fill="url(#rating-badge-bronze-right_svg__j)"
+                d="M8.725 4.987c.63 2.458 1.724 3.464 3.235 3.913.649-.724 1.581-2.529.95-4.986C12.28 1.456 10.598.322 9.679 0c-.457.396-1.584 2.53-.953 4.987"></path>
+          <path fill="url(#rating-badge-bronze-right_svg__k)"
+                d="m12.972 10.003-1.622.81C14.423 22.297 11.034 28.327 0 38.65L1.08 40C11.8 29.33 16.39 23.097 12.973 10.003"></path>
+          <defs>
+            <linearGradient id="rating-badge-bronze-right_svg__a" x1="21.199" x2="-29.801" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-right_svg__b" x1="21.199" x2="-29.801" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-right_svg__c" x1="21.199" x2="-29.801" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-right_svg__d" x1="21.199" x2="-29.801" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-right_svg__e" x1="21.199" x2="-29.801" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-right_svg__f" x1="21.199" x2="-29.801" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-right_svg__g" x1="21.199" x2="-29.801" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-right_svg__h" x1="21.199" x2="-29.801" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-right_svg__i" x1="21.199" x2="-29.801" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-right_svg__j" x1="21.199" x2="-29.801" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+            <linearGradient id="rating-badge-bronze-right_svg__k" x1="21.199" x2="-29.801" y1="20" y2="33"
+                            gradientUnits="userSpaceOnUse">
+              <stop stop-color="#654013"></stop>
+              <stop offset="0.25" stop-color="#E7996E"></stop>
+              <stop offset="0.5" stop-color="#89592C"></stop>
+              <stop offset="0.75" stop-color="#D58D61"></stop>
+              <stop offset="1" stop-color="#D68D62"></stop>
+            </linearGradient>
+          </defs>
+        </svg>
+      </svg>
+    </div>
+    <h5 class="caption">Quality assured</h5>
+  </div>
+</template>
+
+<style scoped>
+
+.quality_container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.leaves_container {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.caption {
+  margin: 0;
+  margin-block-start: -5px;
+  font-size: 16px;
+  text-align: center;
+}
+
+</style>
