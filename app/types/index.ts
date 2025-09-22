e4583b9625659cccd2016cfee277492f00476ad9
Dmitry Makarov <makdmt@gmail.com>
2025-09-22 15:34:02 +0300

    feat(types) add types file

---
+++ app/types/index.ts
@@ -0,0 +1,13 @@
+export type PreviewFormat = 'mobile' | 'desktop';
+
+export type Technologies = 'PWA' | 'React' | 'Vue' | 'Formatjs' | 'Node' | 'Express' | 'Firebase' | 'Tilda';
+
+export type Project = {
+    title: string,
+    description: string,
+    previewFormat: PreviewFormat,
+    srcHigh: string,
+    srcLow?: string,
+    waitSoon?: boolean,
+    technologies?: Technologies[],
+}
