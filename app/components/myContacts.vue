00debc683c38a909cf09d96a4b10795c83545ce7
Dmitry Makarov <makdmt@gmail.com>
2025-09-22 17:40:55 +0300

    feat(components) add myContacts component

---
+++ app/components/myContacts.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="contacts_block">
+    <a href="https://t.me/Makemt" target="_blank">telegram</a>
+    <a href="https://github.com/makdmt" target="_blank">github</a>
+  </div>
+</template>
+
+<style scoped>
+.contacts_block {
+  display: flex;
+  gap: 16px;
+}
+
+@media only screen and (max-width: 725px) {
+  .contacts_block {
+    gap: 6px;
+  }
+}
+
+</style>
