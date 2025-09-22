ba51a15660857379ab8be6a64301b9618bfe28e4
Dmitry Makarov <makdmt@gmail.com>
2025-09-22 17:41:20 +0300

    feat(components) add footer component

--- app/components/footer.vue
+++ app/components/footer.vue
@@ -3,9 +3,30 @@
 </script>
 
 <template>
-<p>Это футер</p>
+  <footer class="container">
+    <QualityAssured/>
+    <div class="signature">
+      <h3 class="author_name">Дмитрий Макаров</h3>
+      <MyContacts style="justify-content: flex-end"/>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
+
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 0;
+  gap: 16px;
+}
+
+.author_name {
+  text-align: right;
+  margin-block-end: 6px;
+}
+
+
 
 </style>
