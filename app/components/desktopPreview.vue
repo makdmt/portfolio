4f5d5b75a23a1d4d100860d2487916ac0446aeca
Dmitry Makarov <makdmt@gmail.com>
2025-09-22 15:00:59 +0300

    feat(components) add desktopPreview component

---
+++ app/components/desktopPreview.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+import {useImagePreload} from "../composables/useImagePreload";
+
+const props = defineProps<{
+  srcHigh: string;
+  srcLow?: string;
+  alt: string;
+  waitSoon?: boolean;
+}>();
+
+const {loaded} = useImagePreload(props.srcHigh);
+
+</script>
+
+<template>
+  <WaitSoon :activate="waitSoon">
+    <img
+        :src="loaded ? srcHigh : srcLow || srcHigh"
+        :alt="alt"
+        class="image"
+    />
+  </WaitSoon>
+</template>
+
+<style scoped>
+
+.image {
+  border: 4px solid var(--white);
+  border-radius: 16px;
+  width: 488px;
+  height: 340px;
+  box-shadow: 17px 17px 48px #0000001f;
+}
+
+@media only screen and (max-width: 900px) {
+  .image {
+    border-radius: 8px;
+    width: 375px;
+    height: 261px;
+  }
+}
+
+</style>
