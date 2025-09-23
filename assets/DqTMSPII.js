b273a6864b0afadf75a96dfac79c52641499554a
Dmitry Makarov <makdmt@gmail.com>
2025-09-23 17:51:14 +0300

    Updates

--- assets/DqTMSPII.js
+++
@@ -1,1 +0,0 @@
-import{P as a,a as s,Q as r,R as u,S as o}from"#entry";function i(e){const n=e||s();return n.ssrContext?.head||n.runWithContext(()=>{if(r()){const t=u(o);if(!t)throw new Error("[nuxt] [unhead] Missing Unhead instance.");return t}})}function d(e,n={}){const t=i(n.nuxt);return a(e,{head:t,...n})}export{d as u};
