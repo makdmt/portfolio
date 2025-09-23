117e94bb133c29bd5debd61b6f8e47cea0086fe4
Dmitry Makarov <makdmt@gmail.com>
2025-09-23 11:44:09 +0300

    feat(components) mobile layout to aboutMe component

--- app/components/aboutMe.vue
+++ app/components/aboutMe.vue
@@ -5,16 +5,28 @@
 <template>
   <div class="container">
     <div class="description_container">
-      <h1 class="username">Дмитрий Макаров</h1>
-      <h3>Fullstack web developer</h3>
-      <p>Разрабатываю web приложения разного уровня сложности: лендинги, pwa для ios и android, crm/erp системы со
+      <div class="username_container">
+        <h1 class="username">Дмитрий Макаров<br/>
+          <span class="subtitle">Fullstack web developer</span>
+        </h1>
+        <img
+            src="/imgs/avatar.webp"
+            alt="avatar"
+            class="image image__mobile"
+        />
+      </div>
+      <p>Разрабатываю web приложения разного уровня сложности: лендинги, pwa для ios и android, crm/erp системы с
+        разделением ролей и
         сложной бизнес логикой.</p>
-      <a class="link" href="#myProjects">Смотреть проекты</a>
+      <div class="links_container">
+        <a class="link" href="#myProjects">Смотреть проекты</a>
+        <MyContacts/>
+      </div>
     </div>
     <img
         src="/imgs/avatar.webp"
         alt="avatar"
-        class="image"
+        class="image image__desktop"
     />
   </div>
 
@@ -33,28 +45,63 @@
   flex-direction: column;
 }
 
+.username_container {
+  display: flex;
+}
+
 .username {
-  max-width: 350px;
-  margin-block-end: 0;
+  max-width: 360px;
+  margin: 0;
+}
+
+.subtitle {
+  display: block;
+  margin-block-start: 18px;
+  font-size: 32px;
+  line-height: 46px;
+}
+
+.links_container {
+  margin-block-start: auto;
+  display: flex;
+  align-items: center;
 }
 
 .link {
-  margin-top: auto;
-  padding: 0;
+  margin-inline-end: 16px;
+  padding: 8px 0;
 }
 
 .image {
   border: 4px solid var(--white);
   border-radius: 16px;
+  box-shadow: 17px 17px 48px #0000001f;
+}
+
+.image__desktop {
   width: 222px;
   height: 222px;
-  box-shadow: 17px 17px 48px #0000001f;
-  margin: 8px;
+}
+
+.image__mobile {
+  display: none;
+  width: 90px;
+  height: 90px;
+  margin-block-start: 12px;
+  margin-inline-start: -105px;
 }
 
 @media only screen and (max-width: 725px) {
   .container {
     flex-wrap: wrap;
+  }
+
+  .image__desktop {
+    display: none;
+  }
+
+  .image__mobile {
+    display: block;
   }
 }
 </style>
