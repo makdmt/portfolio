0e2abd64aa82d79d7c7d5f53853ac81365084bb4
Dmitry Makarov <makdmt@gmail.com>
2025-09-23 15:15:58 +0300

    feat(app) add head tags to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: 'ru',
+            },
+            title: 'Дмитрий Макаров. Портфолио',
+            link: [
+                {rel: 'icon', type: 'image/x-icon', href: '/favicon.png'}
+            ]
+        },
+    },
 })
