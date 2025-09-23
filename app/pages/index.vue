276792496c138ce5da88f8f1f478b2944fdb40da
Dmitry Makarov <makdmt@gmail.com>
2025-09-23 13:59:34 +0300

    feat(pages) add main page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,10 +3,29 @@
 </script>
 
 <template>
-  <Header />
-  <Footer />
+  <Header/>
+  <main>
+    <AboutMe id="aboutMe"/>
+    <MyProjects id="myProjects"/>
+  </main>
+  <Footer id="footer"/>
 </template>
 
 <style scoped>
+
+#aboutMe {
+  padding-block-start: 76px;
+  padding-block-end: 58px;
+}
+
+#myProjects {
+  scroll-margin-block: 32px;
+}
+
+@media only screen and (max-width: 725px) {
+  #aboutMe {
+    padding-block-start: 70px;
+  }
+}
 
 </style>
