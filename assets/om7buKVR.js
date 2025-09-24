6bf31249bef0c6f75bbaa8adaa51b83f09581eb3
Dmitry Makarov <makdmt@gmail.com>
2025-09-24 12:09:44 +0300

    Updates

--- assets/om7buKVR.js
+++
@@ -1,1 +0,0 @@
-import{Q as a,a as s,R as r,S as u,T as o}from"#entry";function i(e){const n=e||s();return n.ssrContext?.head||n.runWithContext(()=>{if(r()){const t=u(o);if(!t)throw new Error("[nuxt] [unhead] Missing Unhead instance.");return t}})}function d(e,n={}){const t=i(n.nuxt);return a(e,{head:t,...n})}export{d as u};
